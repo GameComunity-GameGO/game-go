3955640fe6936d3ef90a1db10fdae12cd25b9b01
Siby1lA <psb4644@gmail.com>
2022-07-10 12:13:37 +0900

    feature: 이미지 슬라이더 기능 추가

---
+++ src/utils/images.tsx
@@ -0,0 +1,5 @@
+export const images = [
+  "https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/bltb16f7bf3691493f5/5e32376ae147ae4537d93a32/LOL_PROMOART_5.jpg",
+  "https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/bltdf6f30def332c3df/6298387236712c4150de038a/060722_Patch_12_11_Notes_Banner.jpg",
+  "https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/blt55da176a88f21fd2/5d788cbedb54493ebd97f1c6/LOL_PROMOART_14.jpg",
+];
