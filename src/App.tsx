a37f65cb08522fc16f6984b35fe984290cd4a059
bm4656 <newjin46@gmail.com>
2022-07-21 00:38:44 +0900

    마이페이지 기초 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import GamePage from "./Routes/GamePage";
+import MyPage from "./Routes/MyPage";
 
 import Main from "./Routes/Main";
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Main />}></Route>
         <Route path="/gamepage/:game" element={<GamePage />}></Route>
+        <Route path="/mypage" element={<MyPage />}></Route>
       </Routes>
     </Router>
   );
--- src/Routes/MyPage.tsx
+++ src/Routes/MyPage.tsx
@@ -1,7 +1,49 @@
-import React from "react";
+import { useState } from "react";
+import styled from "styled-components";
+import Board from "../components/Board";
+import { useParams } from "react-router-dom";
+
+const Wrap = styled.div`
+  width: 80vw;
+  height: 100vh;
+  min-width: 700px;
+`;
+const Title = styled.div`
+  margin-top: 40px;
+  font-weight: 600;
+  font-size: 26px;
+  margin-left: 40px;
+`;
+const Label = styled.div`
+  margin-top: 40px;
+  font-weight: 60;
+  font-size: 26px;
+  margin-right: 40px;
+`;
+const Ul = styled.ul`
+  list-style: none;
+  padding: 0px;
+`;
+const Li = styled.li`
+  width: 28vw;
+  display: flex;
+  align-items: center;
+  font-size: 1.6vw;
+  margin 0 auto;
+`;
 
 function MyPage() {
-  return <div></div>;
-}
+  return (
+    <Wrap>
+      <Title>마이 페이지</Title>
+      <Ul>
+      <Li>
+      <Label>닉네임</Label>
+      <Label>성별</Label>
+      </Li>  
+      </Ul>
+    </Wrap>
+  );
+  }
 
 export default MyPage;
