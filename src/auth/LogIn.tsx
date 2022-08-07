5e9fad84211efb32025b7506ab8260a0da5b9458
bm4656 <newjin46@gmail.com>
2022-08-08 02:57:13 +0900

    bug: refreshToken으로 accessToken 재발급 오류

--- src/auth/LogIn.tsx
+++ src/auth/LogIn.tsx
@@ -16,6 +16,7 @@ import {
   Button2,
 } from "./styles";
 import { setSignUpToggle } from "../redux/action";
+import { access } from "fs";
 interface IForm {
   email: string;
   password: string;
@@ -51,17 +52,7 @@ function LogIn() {
         }),
         config
       )
-      .then((response) => {
-        const { authorization, authorization_refresh } = response.headers;
-        axios.defaults.headers.common[
-          "Authorization"
-        ] = `Bearer ${authorization}`;
-        axios.defaults.headers.common[
-          "Authorization-refresh"
-        ] = `Bearer ${authorization_refresh}`;
-        console.log(response);
-        jwtDecode(authorization);
-      })
+      .then(loginSuccess)
       .catch((error) => {
         console.log(error);
         // 임시 에러 처리
@@ -70,10 +61,59 @@ function LogIn() {
         // });
       });
   };
+  //jwt 받아오는 decode
   const jwtDecode = (token: any) => {
-    var decoded = jwt_decode(token);
-    console.log(decoded);
+    const decoded: any = jwt_decode(token);
+
+    console.log(decoded?.exp);
+    var nowTime = Date.now();
+    var reTime = decoded.exp * 1000;
+    console.log(Date.now());
+    console.log(reTime);
+    // setInterval(nowTime,1000)
+    if (Date.now() < reTime) {
+      onSilentRefresh();
+    }
   };
+  async function onSilentRefresh() {
+    console.log("엑세스 토큰 시간 만료");
+    const data = await localStorage.getItem("refreshToken");
+    console.log(data);
+    axios
+      .post("/api/v1/accessToken", {
+        headers: { Authorization_refresh: `Bearer ${data}` },
+      })
+      .then((response) => {
+        // axios.defaults.headers.common[
+        //   //   "Authorization_refresh"
+        //   // ] = `Bearer ${data}`;
+        //   "aaa"
+        // ] = `123`;
+        console.log(response);
+        // jwtDecode(response.data);
+      })
+      .catch((error) => {});
+  }
+  //로그인 성공 시
+  async function loginSuccess(response: any) {
+    function sleep(ms: any) {
+      return new Promise((resolve) => setTimeout(resolve, ms));
+    }
+    localStorage.removeItem("accessToken");
+    localStorage.removeItem("refreshToken");
+    const { authorization, authorization_refresh } = response.headers;
+    // axios.defaults.headers.common["Authorization"] = `Bearer ${authorization}`;
+    // axios.defaults.headers.common[
+    //   "Authorization-refresh"
+    // ] = `Bearer ${authorization_refresh}`;
+    localStorage.setItem("accessToken", authorization);
+    localStorage.setItem("refreshToken", authorization_refresh);
+    console.log(response);
+    await sleep(3000);
+    jwtDecode(authorization);
+    // setTimeout(onSilentRefresh);
+  }
+
   const dispatch = useDispatch();
   const toggleHandler = () => {
     dispatch(setSignUpToggle(false));
@@ -100,6 +140,7 @@ function LogIn() {
       <LinkContainer>
         아직 회원이 아니신가요?&nbsp;
         <Button2 onClick={toggleHandler}>회원가입 하러가기</Button2>
+        {/* <Button2 onClick={jwtDecode}>jwtjwt</Button2> */}
       </LinkContainer>
     </Container>
   );
