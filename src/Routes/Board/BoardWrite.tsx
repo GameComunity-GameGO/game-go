72eb95d9bea32f94ed5718dba6435ddb85f54a0a
Siby1lA <psb4644@gmail.com>
2022-08-24 00:26:30 +0900

    feature: 에디터 이미지 추가시 url불러오기

--- src/Routes/Board/BoardWrite.tsx
+++ src/Routes/Board/BoardWrite.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useMemo, useRef } from "react";
 import { useForm } from "react-hook-form";
 import ReactQuill from "react-quill";
 import { useLocation, useNavigate, useParams } from "react-router-dom";
@@ -9,42 +9,6 @@ import "react-quill/dist/quill.snow.css";
 import { useDispatch } from "react-redux";
 import axios from "axios";
 
-const toolbarOptions = [
-  ["link", "image", "video"],
-  [{ header: [1, 2, 3, false] }],
-  ["bold", "italic", "underline", "strike"],
-  [{ list: "ordered" }, { list: "bullet" }],
-  [{ color: [] }, { background: [] }],
-  [{ align: [] }],
-];
-
-// 옵션에 상응하는 포맷, 추가해주지 않으면 text editor에 적용된 스타일을 볼수 없음
-export const formats = [
-  "header",
-  "font",
-  "size",
-  "bold",
-  "italic",
-  "underline",
-  "strike",
-  "align",
-  "blockquote",
-  "list",
-  "bullet",
-  "indent",
-  "background",
-  "color",
-  "link",
-  "image",
-  "video",
-  "width",
-];
-
-const modules = {
-  toolbar: {
-    container: toolbarOptions,
-  },
-};
 const Wrap = styled.div`
   min-width: 1000px;
   display: flex;
@@ -167,6 +131,94 @@ interface IBoardWrite {
   image: string;
 }
 function BoardWrite() {
+  const toolbarOptions = [
+    ["link", "image", "video"],
+    [{ header: [1, 2, 3, false] }],
+    ["bold", "italic", "underline", "strike"],
+    [{ list: "ordered" }, { list: "bullet" }],
+    [{ color: [] }, { background: [] }],
+    [{ align: [] }],
+  ];
+
+  const imageHandler = () => {
+    console.log("에디터에서 이미지 버튼을 클릭하면 이 핸들러가 시작됩니다!");
+
+    // 1. 이미지를 저장할 input type=file DOM을 만든다.
+    const input: any = document.createElement("input");
+    // 속성 써주기
+    input.setAttribute("type", "file");
+    input.setAttribute("accept", "image/*");
+    input.click(); // 에디터 이미지버튼을 클릭하면 이 input이 클릭된다.
+    // input이 클릭되면 파일 선택창이 나타난다.
+
+    // input에 변화가 생긴다면 = 이미지를 선택
+    input.addEventListener("change", async () => {
+      console.log("사진 첨부");
+      const file = input.files[0];
+      // multer에 맞는 형식으로 데이터 만들어준다.
+      const formData = new FormData();
+
+      formData.append("img", file); // formData는 키-밸류 구조
+      // 백엔드 multer라우터에 이미지를 보낸다.
+      console.log(file);
+      try {
+        // const result = await axios.post("http://localhost:4050/img", formData);
+        const result = await axios.post(`/api/`, formData, config);
+        console.log("성공 시, 백엔드가 보내주는 데이터", result.data.url);
+        const IMG_URL = result.data.url;
+        // 이 URL을 img 태그의 src에 넣은 요소를 현재 에디터의 커서에 넣어주면 에디터 내에서 이미지가 나타난다
+        // src가 base64가 아닌 짧은 URL이기 때문에 데이터베이스에 에디터의 전체 글 내용을 저장할 수있게된다
+        // 이미지는 꼭 로컬 백엔드 uploads 폴더가 아닌 다른 곳에 저장해 URL로 사용하면된다.
+
+        // 이미지 태그를 에디터에 써주기 - 여러 방법이 있다.
+        const editor = quillRef.current.getEditor(); // 에디터 객체 가져오기
+        // 1. 에디터 root의 innerHTML을 수정해주기
+        // editor의 root는 에디터 컨텐츠들이 담겨있다. 거기에 img태그를 추가해준다.
+        // 이미지를 업로드하면 -> 멀터에서 이미지 경로 URL을 받아와 -> 이미지 요소로 만들어 에디터 안에 넣어준다.
+        // editor.root.innerHTML =
+        //   editor.root.innerHTML + `<img src=${IMG_URL} /><br/>`; // 현재 있는 내용들 뒤에 써줘야한다.
+
+        // 2. 현재 에디터 커서 위치값을 가져온다
+        const range = editor.getSelection();
+        // 가져온 위치에 이미지를 삽입한다
+        editor.insertEmbed(range.index, "image", IMG_URL);
+      } catch (error) {
+        console.log("실패했어요ㅠ");
+      }
+    });
+  };
+  // 옵션에 상응하는 포맷, 추가해주지 않으면 text editor에 적용된 스타일을 볼수 없음
+  const modules = useMemo(() => {
+    return {
+      toolbar: {
+        container: toolbarOptions,
+        handlers: {
+          // 이미지 처리는 우리가 직접 imageHandler라는 함수로 처리할 것이다.
+          image: imageHandler,
+        },
+      },
+    };
+  }, []);
+  const formats = [
+    "header",
+    "font",
+    "size",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "align",
+    "blockquote",
+    "list",
+    "bullet",
+    "indent",
+    "background",
+    "color",
+    "link",
+    "image",
+    "video",
+    "width",
+  ];
   const config = {
     headers: {
       "Content-Type": "application/json",
@@ -174,6 +226,8 @@ function BoardWrite() {
     },
     withCredentials: true,
   };
+  const quillRef = useRef<any>();
+
   const {
     register,
     handleSubmit,
@@ -206,6 +260,7 @@ function BoardWrite() {
           title: title,
           contents: contents,
           category: type,
+          type: select,
         }),
         config
       )
@@ -216,6 +271,7 @@ function BoardWrite() {
         console.log(error);
       });
   };
+
   return (
     <Wrap>
       <CategoryNav />
@@ -255,6 +311,7 @@ function BoardWrite() {
                 <ReactQuill
                   style={{ height: "430px" }}
                   theme="snow"
+                  ref={quillRef}
                   modules={modules}
                   formats={formats}
                   onChange={handleChange}
